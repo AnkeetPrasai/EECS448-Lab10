594998c60cce65852fd5ede152a8e022461157af
AnkeetPrasai <aprasai@ku.edu>
2021-05-06 13:04:12 -0500

    reupload because github wasnt working

---
+++ deletePost.php
@@ -0,0 +1,37 @@
+<?php	
+	$connection = new mysqli("mysql.eecs.ku.edu", "ankeetprasai", "nai7uk4E", "ankeetprasai");
+
+	if($connection->connect_errno)
+	{
+		printf("Connect failed!!\n", $connection->connect_error);
+		exit();
+	}
+
+	$result = $connection->query("SELECT content, author_id, post_id FROM Posts");
+
+	$count = 1;
+	$selected = $_POST['post'];
+	if($result->num_rows > 0)
+	{
+		while($row = $result->fetch_assoc())
+		{
+			$N = count($selected);
+			for($i = 0; $i < $N; $i++)
+			{
+				if($selected[$i] == $row["post_id"])
+				{
+					$id = $row["post_id"];
+					$delete = $connection->query("DELETE FROM Posts WHERE post_id ='".$id."'");
+					echo "You deleted the post with the id " .$id. "<br>";
+				}	
+			}		
+			$count++;
+		}
+	}
+	else
+	{
+		echo"There are no posts!";
+	}
+	$connection -> close();
+
+?>
